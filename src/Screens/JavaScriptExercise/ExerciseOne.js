579d73888f4058a41a61b357ccaceffa39001161
anuredapple2014 <anurag@redappletech.com>
2022-10-19 19:06:27 +0530

    commit 10

--- src/Screens/JavaScriptExercise/ExerciseOne.js
+++ src/Screens/JavaScriptExercise/ExerciseOne.js
@@ -13,12 +13,40 @@ import React from 'react'
   console.log(isEqualTo100(0,100))
   console.log(isEqualTo100(19,0))
   console.log(isEqualTo100(0,10))
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
 /* 2-Write a JavaScript Program to get the extension of a filename */
 const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
-
 console.log(getFileExtension('anurag.singh'))
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*3 Write a JavaScript program to replace every character in a given string with the character following it in
+the alphabet */
+
+/*Here we have to use few JavaScript Function 
+String.fromCharCode
+charCodeAt*/
+
+const moveCharsForward = (str) =>str
+                             .split('')
+                             .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+                             .join('');
+         console.log(moveCharsForward('abcde'))                    
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*4 Write a Javascript Program to get the current date.
+
+Expected Output :
+ mm-dd-yyyy , mm/dd/yyyy or dd-mm-yyyy , dd/mm/yyyy     */
+ 
+ const formatDate = (date = new Date()) =>{
+  const days =date.getDate(); 
+  const months = date.getMonth();
+  const years = date.getFullYear();
+  return `${days}-${months}-${years}`
+ }
+console.log(formatDate())
 
 const ExerciseOne = () => {
   return (
